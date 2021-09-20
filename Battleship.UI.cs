8df9b496bb6ceb50d876d5da13d7912110b6df73
Eric Valek <ericjvalekgmail.com>
2021-09-19 22:24:52 -0400

    first commit

---
+++ Battleship.UI.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Battleship_Reboot
+{
+    class BattleshipUI
+    {
+        public void Run()
+        {
+
+        }
+        public void MainMenu()
+        {
+
+        }
+    }
+}
